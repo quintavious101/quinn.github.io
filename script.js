4adf20463c29aff6b6269acfaba96b5a32bcd696
quintavious101 <coll1062@msu.edu>
2025-07-30 15:47:43 -0400

    debugging dark mode toggle

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("darkModeToggle");
   const darkModeEnabled = localStorage.getItem("darkMode") === "enabled";
 
+console.log("Dark mode enabled? ", darkModeEnabled);
+
   if (darkModeEnabled) {
     document.body.classList.add("dark-mode");
   }
 
-  toggle.addEventListener("click", function () {
+   toggle.addEventListener("click", function () {
+    console.log("Button clicked");
+
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
       localStorage.setItem("darkMode", "enabled");
+      console.log("Dark mode enabled");
     } else {
       localStorage.setItem("darkMode", "disabled");
+      console.log("Dark mode disabled");
     }
   });
 });
